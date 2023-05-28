be8037daa043d94f4fad454f03976a8ffc3a4c62
Kingsley Ramos <kingsleyramos@gmail.com>
2023-05-28 08:14:18 -0700

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,9 @@
 import {ApolloServer} from '@apollo/server';
 import {startStandaloneServer} from '@apollo/server/standalone';
+import typeDefs from './typeDefs';
+import * as dotenv from 'dotenv';
 
-// A schema is a collection of type definitions (hence "typeDefs")
-// that together define the "shape" of queries that are executed against
-// your data.
-const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-`;
+dotenv.config();
 
 const books = [
     {
@@ -48,9 +32,9 @@ const server = new ApolloServer({
     introspection: true,
 });
 
-const port = Number.parseInt(process.env.PORT) || 4000;
-
 async function startServer() {
+    const port: number = Number(process.env.PORT) || 4000;
+
     // Passing an ApolloServer instance to the `startStandaloneServer` function:
     //  1. creates an Express app
     //  2. installs your ApolloServer instance as middleware
